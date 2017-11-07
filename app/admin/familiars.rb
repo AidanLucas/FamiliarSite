27f39832a8e77b3917d977eee11fe6950043bde0
Unknown <alucas44@academic.rrc.ca>
2017-11-07 14:14:14 -0600

    Recreated my ten products

--- app/admin/familiars.rb
+++ app/admin/familiars.rb
@@ -4,9 +4,9 @@ ActiveAdmin.register Familiar do
   index do
     selectable_column
     id_column
-    column 'Name'
-    column 'Description'
-    column 'Price'
+    column 'Name', :name
+    column 'Description', :description
+    column 'Price', :price
     column 'Image', sortable: :image_file_name do |familiar| link_to familiar.image_file_name, familiar.image.url end
     column :created_at
     actions
@@ -17,7 +17,7 @@ ActiveAdmin.register Familiar do
       f.input :name
       f.input :description
       f.input :price
-      f.input :image, required: true, as: :file      
+      f.input :image, required: true, as: :file
     end
   f.actions
   end
