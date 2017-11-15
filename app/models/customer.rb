524e578e8c2600b15b9255fece44c1bbc6b0f1fc
Unknown <alucas44@academic.rrc.ca>
2017-11-14 22:13:14 -0600

    Validation for the Customers Table

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  validates :username, presence: true, uniqueness: true, length: { in: 8..15 }
+  validates :password, presence: true, uniqueness: true, confirmation: true, length: { in: 8..20 }
+  validates :email, presence: true, uniqueness: true, confirmation: true,
+            format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :province, presence: true
 end
