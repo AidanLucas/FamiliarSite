d592145e260c4f89ba88f89bca81f424c271a856
Unknown <alucas44@academic.rrc.ca>
2017-11-14 20:24:28 -0600

    Validation for the Familiar Table

--- app/models/familiar.rb
+++ app/models/familiar.rb
@@ -1,7 +1,14 @@
 class Familiar < ApplicationRecord
+  #Assoications
   has_many :familiar_categories
   has_many :categories, through: :familiar_categories
 
+  #Validation
+  validates :name, presence: true,  uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true,  uniqueness: true, numericality: true
+
+  #Paperclip
   has_attached_file :image
   validates_attachment_file_name :image, :matches => [/png\z/, /jpe?g\z/]
   validates_uniqueness_of :image_file_name
